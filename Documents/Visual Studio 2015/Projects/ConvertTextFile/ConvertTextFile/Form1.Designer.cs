21cf06ba8f6db9ddb8fa27ccf1f0f690c41f2855
Paulo Santiago <pjrsantiago@gmail.com>
2019-04-05 23:14:08 -0300

    Primeira versão do ConvertTextFile

---
+++ Documents/Visual Studio 2015/Projects/ConvertTextFile/ConvertTextFile/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ConvertTextFile
+{
+    partial class frmHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblArquivoOrigem = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtArqivoOrigem = new System.Windows.Forms.TextBox();
+            this.btnEscolheArquivo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtArquivoDestino = new System.Windows.Forms.TextBox();
+            this.btnArquivoDestino = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnConverter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblArquivoOrigem
+            // 
+            this.lblArquivoOrigem.AutoSize = true;
+            this.lblArquivoOrigem.Location = new System.Drawing.Point(13, 13);
+            this.lblArquivoOrigem.Name = "lblArquivoOrigem";
+            this.lblArquivoOrigem.Size = new System.Drawing.Size(95, 13);
+            this.lblArquivoOrigem.TabIndex = 0;
+            this.lblArquivoOrigem.Text = "Arquivo de origem:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // txtArqivoOrigem
+            // 
+            this.txtArqivoOrigem.Location = new System.Drawing.Point(16, 30);
+            this.txtArqivoOrigem.Name = "txtArqivoOrigem";
+            this.txtArqivoOrigem.ReadOnly = true;
+            this.txtArqivoOrigem.Size = new System.Drawing.Size(386, 20);
+            this.txtArqivoOrigem.TabIndex = 1;
+            // 
+            // btnEscolheArquivo
+            // 
+            this.btnEscolheArquivo.Location = new System.Drawing.Point(409, 30);
+            this.btnEscolheArquivo.Name = "btnEscolheArquivo";
+            this.btnEscolheArquivo.Size = new System.Drawing.Size(95, 23);
+            this.btnEscolheArquivo.TabIndex = 2;
+            this.btnEscolheArquivo.Text = "Escolher arquivo";
+            this.btnEscolheArquivo.UseVisualStyleBackColor = true;
+            this.btnEscolheArquivo.Click += new System.EventHandler(this.btnEscolheArquivo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Arquivo de destino:";
+            // 
+            // txtArquivoDestino
+            // 
+            this.txtArquivoDestino.Location = new System.Drawing.Point(16, 85);
+            this.txtArquivoDestino.Name = "txtArquivoDestino";
+            this.txtArquivoDestino.ReadOnly = true;
+            this.txtArquivoDestino.Size = new System.Drawing.Size(386, 20);
+            this.txtArquivoDestino.TabIndex = 4;
+            // 
+            // btnArquivoDestino
+            // 
+            this.btnArquivoDestino.Location = new System.Drawing.Point(409, 85);
+            this.btnArquivoDestino.Name = "btnArquivoDestino";
+            this.btnArquivoDestino.Size = new System.Drawing.Size(95, 23);
+            this.btnArquivoDestino.TabIndex = 5;
+            this.btnArquivoDestino.Text = "Arquivo Destino";
+            this.btnArquivoDestino.UseVisualStyleBackColor = true;
+            this.btnArquivoDestino.Click += new System.EventHandler(this.btnArquivoDestino_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // btnConverter
+            // 
+            this.btnConverter.Location = new System.Drawing.Point(205, 139);
+            this.btnConverter.Name = "btnConverter";
+            this.btnConverter.Size = new System.Drawing.Size(95, 23);
+            this.btnConverter.TabIndex = 6;
+            this.btnConverter.Text = "Converter";
+            this.btnConverter.UseVisualStyleBackColor = true;
+            this.btnConverter.Click += new System.EventHandler(this.btnConverter_Click);
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 177);
+            this.Controls.Add(this.btnConverter);
+            this.Controls.Add(this.btnArquivoDestino);
+            this.Controls.Add(this.txtArquivoDestino);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEscolheArquivo);
+            this.Controls.Add(this.txtArqivoOrigem);
+            this.Controls.Add(this.lblArquivoOrigem);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Converter Arquivo Texto";
+            this.Load += new System.EventHandler(this.frmHome_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblArquivoOrigem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtArqivoOrigem;
+        private System.Windows.Forms.Button btnEscolheArquivo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtArquivoDestino;
+        private System.Windows.Forms.Button btnArquivoDestino;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnConverter;
+    }
+}
+
---
+++ Documents/Visual Studio 2015/Projects/ConvertTextFile/ConvertTextFile/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertTextFile
+{
+    public partial class frmHome : Form
+    {
+        public frmHome()
+        {
+            InitializeComponent();
+        }
+
+        private void frmHome_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEscolheArquivo_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            txtArqivoOrigem.Text = openFileDialog1.FileName;
+        }
+
+        private void btnArquivoDestino_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            txtArquivoDestino.Text = saveFileDialog1.FileName;
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            if (valida())
+            {
+                executaArquivoFuncionario();
+            }else
+            {
+                MessageBox.Show("Informe os arquivos de origem e destino!", "Erro ao gerar arquivo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //executaArquivoFuncionario();
+        }
+
+        private bool valida()
+        {
+            if((txtArqivoOrigem.Text != "") & (txtArquivoDestino.Text != ""))
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+
+        }
+
+        private void executaArquivoFuncionario()
+        {
+
+            try
+            {
+                string CaminhoOrigem = txtArqivoOrigem.Text;
+                string CaminhoDestino = txtArquivoDestino.Text;
+                //string CaminhoOrigem = "E:\\Pedro Barboza\\Downloads\\Ficha Resgistro_iliane.txt";
+                //string CaminhoDestino = "E:\\Pedro Barboza\\Downloads\\Teste.txt";
+
+                StreamReader str;
+                StreamWriter stw = new StreamWriter(File.Open(CaminhoDestino, FileMode.CreateNew), Encoding.Default);
+
+                string[] cabecalho = new string[80] { "HESCHOR_ADMISSAO", "EMPRESA", "NOME", "ORDEM", "REGISTRO", "IMPRIMEFOTO", "END", "TELEFONE",
+                "CEP","MUNIC","UF","ESTCIVIL","DATNASC","SEXO","INSTRUCAO","NAC","ANO","CTPS","SERIE","MOD","RG","ORGAO",
+                "ESTEMIS","DATA","VINCFGTS","PIS","CIC","DATADM","TITULO","CNH","FUNCAO","CBO","PAGTO","TIP","SAL","HORARIO",
+                "INTERVALO","PAI","MAE","REL - F","NACPAI","NACMAE","CERTRES","OBS","ISENTOPONTO","BRANCO","DATFICHA",
+                "CR","HABPROF","NOM - CONREG","NUM - CONREG","REG - CONREG","SIG - CONREG","OCIPA","TESTE",
+                "CNPJ - CAR","NOMCOMER - CAR","END - CAR","BAIRRO - CAR","CEP - CAR","CIDADE - CAR","CIDADE - ASSINATURA",
+                "DEMISSAO - ASSINATURA","UF - CAR","CARIMBO","BAIRRO","MUNIC - NASC","UFNASC","DOCIDENTIFRNE",
+                "DTVALIDRNE","TPVISTO","DTVALIDCTPS","DATCARTRABALHO","DATPIS","CARGO","ADICINS","ADICPER","ASSINA",
+                "PFISICA","REGEMP"};
+
+                string cabecalho2 = "";
+                for (int i = 0; i < 80; i++)
+                {
+                    if (i < 79)
+                    {
+                        cabecalho2 = cabecalho2 + cabecalho[i] + ";";
+                    }
+                    else
+                    {
+                        cabecalho2 = cabecalho2 + cabecalho[i];
+                    }
+
+                }
+
+                str = File.OpenText(CaminhoOrigem);
+                //stw = File.CreateText(CaminhoDestino);
+
+                stw.WriteLine(cabecalho2);
+
+                string linha_ler = "";
+                string linha_gravar = "";
+                int posicao;
+                int contador = 0;
+                bool ativador = true;
+                string linha_anterior = "0!0";
+
+                while (str.EndOfStream != true)
+                {
+                    linha_ler = str.ReadLine();
+                    posicao = linha_ler.IndexOf("!");
+                    if (posicao > 0)
+                    {
+                        if (linha_ler == "FIM_VISUALIZADOR!")
+                        {
+                            ativador = false;
+                        }
+                        else if (linha_anterior == "FIM!")
+                        {
+                            ativador = true;
+                        }
+                    }
+                    if (ativador)
+                    {
+                        if (posicao < 0 || linha_ler.Substring(0, posicao) == cabecalho[contador])
+                        {
+                            if (contador < 79)
+                            {
+                                linha_gravar = linha_gravar + linha_ler.Substring(posicao + 1) + ";";
+                                contador = contador + 1;
+                            }
+                            else
+                            {
+                                linha_gravar = linha_gravar + linha_ler.Substring(posicao + 1);
+                                stw.WriteLine(linha_gravar);
+                                linha_gravar = "";
+                                contador = 0;
+                            }
+                        }
+                        else
+                        {
+                            if (contador < 79)
+                            {
+                                linha_gravar = linha_gravar + ";";
+                                contador = contador + 1;
+                            }
+
+                        }
+                    }
+                    linha_anterior = linha_ler;
+                }
+
+                str.Close();
+                stw.Close();
+
+                MessageBox.Show("A geração do arquivo foi concluída com sucesso!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro durante a geração do arquivo! " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+            
+        }
+
+    }
+}
